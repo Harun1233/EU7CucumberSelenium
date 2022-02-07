60fbddf01c473c362b623a591ee4c07117c3b81d
Harun1233 <av.harunonat@gmail.com>
2022-02-07 10:56:17 +0000

    something

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -7,14 +7,15 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"json:target/cucumber.json"},
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports"
+        },
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
         tags = "@wip"
 )
 public class CukesRunner {
-
 
 
 }
